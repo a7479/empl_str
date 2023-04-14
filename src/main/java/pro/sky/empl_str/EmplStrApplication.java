d5ccff4b8db278d5d21e30fe8d49eea17dfb6597
Александр <perchatki79@gmail.com>
2023-04-14 14:40:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/empl_str/EmplStrApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.empl_str;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplStrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplStrApplication.class, args);
+	}
+
+}
